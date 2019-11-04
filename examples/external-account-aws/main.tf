97e108eff2e03986a47b6dab2518194412232548
jrbeverly <Jonathan.Beverly@D2L.com>
2019-11-04 15:44:24 -0500

    Example works with existing AWS

--- examples/external-account-aws/main.tf
+++ examples/external-account-aws/main.tf
@@ -1,13 +1,16 @@
-resource "random_uuid" "evident_external_id" {}
-
 resource "evident_external_account" "default" {
   name        = "EvidentProviderTest"
-  external_id = "${random_uuid.evident_external_id.result}"
-  arn         = "${var.role_arn}"
-  team_id     = "${var.team_id}"
+  external_id = var.external_id
+  arn         = var.role_arn
+  team_id     = var.team_id
 }
 
 variable "team_id" {
+  type        = string
+  description = "The ID of the team the external account belongs to"
+}
+
+variable "external_id" {
   type        = string
   description = "The ID of the team the external account belongs to"
 }
