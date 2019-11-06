7dc0268af67325de3f7a51f19434279d66ac4340
Jonathan Beverly <jrbeverly@users.noreply.github.com>
2019-11-06 09:53:11 -0500

    Replace HTTP client with rest client (resty) (#34)
    * Add placeholder examples for each method
    
    * Set example headers
    
    * Ignore environment files
    
    * Resolve issues with create
    
    * Swap delete over in simple form
    
    * Resty on update & all
    
    * Remove trailing slash

---
+++ tests/rate-limit/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Brightspace/terraform-provider-evident/evident/api"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Missing ID for evident resource")
+	}
+	arg := os.Args[1]
+
+	client := api.Evident{
+		Credentials: api.Credentials{
+			AccessKey: []byte(os.Getenv("EVIDENT_ACCESS_KEY")),
+			SecretKey: []byte(os.Getenv("EVIDENT_SECRET_KEY")),
+		},
+		RetryMaximum: 5,
+	}
+
+	result, _ := client.Get(arg)
+	fmt.Println("id:\n", result.ID)
+	fmt.Println("name:\n", result.Attributes.Name)
+	fmt.Println("arn:\n", result.Attributes.Arn)
+	fmt.Println("external_id:\n", result.Attributes.ExternalID)
+}
